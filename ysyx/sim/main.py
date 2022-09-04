f307c3ca415a7341235c81cb6ac8328a2606f577
maksyuki <maksyuki@126.com>
2022-09-04 21:12:39 +0800

    feat: add regession test script

--- ysyx/sim/main.py
+++ ysyx/sim/main.py
@@ -2,13 +2,13 @@
 
 import os
 
-app = ['hello', 'memtest', 'rtthread']
-
+app_type = ['flash', 'loader']
+app = [('hello', 20), ('memtest', 50), ('rtthread', 350)]
 
 def run_test():
-    for v in app:
-        print(v)
-    os.system('make test')
+    for i in app_type:
+        for j in app:
+            os.system('make SOC_APP_TYPE=' + i + ' SOC_APP_NAME=' + j[0] + ' SOC_SIM_TIME=' + str(j[1]) + ' test')
 
 
 run_test()
