44e06c472f5970b80bb0334457335e91c9ffe4c2
maksyuki <maksyuki@126.com>
2022-09-09 18:40:41 +0800

    feat: add screen flash func impl

--- ysyx/sim/src/MediaWindow.hpp
+++ ysyx/sim/src/MediaWindow.hpp
@@ -1,3 +1,4 @@
+#include <ctime>
 #include <iostream>
 #include <SDL2/SDL.h>
 
@@ -16,43 +17,51 @@ public:
         }
 
         win = SDL_CreateWindow("PS/2 & VGA Test",
-                                  0,
-                                  2500,
-                                  640,
-                                  480,
-                                  SDL_WINDOW_SHOWN);
+                               0,
+                               2500,
+                               400,
+                               300,
+                               SDL_WINDOW_SHOWN);
+        rdr = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+        std::memset(fb, 0, sizeof(fb));
     }
 
     void run()
     {
         bool gameIsRunning = true;
+        int y = 10;
         while (gameIsRunning)
         {
             SDL_Event event;
-            // Start our event loop
             while (SDL_PollEvent(&event))
             {
                 if (event.type == SDL_QUIT)
                 {
                     gameIsRunning = false;
                 }
-                else if (event.type == SDL_KEYDOWN)
+                const Uint8 *kdbState = SDL_GetKeyboardState(NULL);
+                if (kdbState[SDL_SCANCODE_RIGHT])
                 {
-                    std::cout << "a key has been pressed" << std::endl;;
-                    if (event.key.keysym.sym == SDLK_0)
-                    {
-                        std::cout << "0 was pressed" << std::endl;;
-                    }
-                }
-                // Retrieve the state of all of the keys
-                // Then we can query the scan code of one or more
-                // keys at a time
-                const Uint8 *state = SDL_GetKeyboardState(NULL);
-                if (state[SDL_SCANCODE_RIGHT])
-                {
-                    std::cout << "right arrow key is pressed" << std::endl;;
+                    std::cout << "right arrow key is pressed" << std::endl;
                 }
             }
+
+            SDL_SetRenderDrawColor(rdr, 255, 0, 0, SDL_ALPHA_OPAQUE);
+            SDL_RenderClear(rdr);
+
+            start = std::clock();
+            SDL_SetRenderDrawColor(rdr, std::rand() % 256, std::rand() % 256, std::rand() % 256, SDL_ALPHA_OPAQUE);
+            for (int i = 0; i < 400; ++i)
+            {
+                for (int j = 0; j < 300; ++j)
+                {
+                    SDL_RenderDrawPoint(rdr, i, j);
+                }
+            }
+
+            SDL_RenderPresent(rdr);
+
+            std::cout << "pref: "<< int (CLOCKS_PER_SEC * 1.0 / double(std::clock() - start)) << "fps" <<std::endl;
         }
 
         SDL_DestroyWindow(win);
@@ -62,4 +71,7 @@ public:
 
 private:
     SDL_Window *win = nullptr;
+    SDL_Renderer *rdr = nullptr;
+    clock_t start;
+    int fb[640][48];
 };
