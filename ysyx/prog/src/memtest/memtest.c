85281ebdf8a715a9cbdcc4f7ad97d0bc9fc5fd8d
maksyuki <maksyuki@126.com>
2022-10-02 19:51:26 +0800

    feat: add cond comp statement

--- ysyx/prog/src/memtest/memtest.c
+++ ysyx/prog/src/memtest/memtest.c
@@ -2,14 +2,19 @@
 #include <klib-macros.h>
 #include <klib.h>
 
+#ifdef SDRAM_LOAD
+#define DAT_ADDR 0xFC200000
+#else
+#define DAT_ADDR 0x80200000
+#endif
+
 #define ARR_SIZE 0x2000
 uint16_t data[ARR_SIZE];
 
 int main(const char *args)
 {
     putstr("start test...\n");
-    uint16_t *data = (void *)(0x80200000);
-    //  uint16_t* data = (void*)(0xfc200000);
+    uint16_t *data = (void *)(DAT_ADDR);
     for (int i = 0; i < ARR_SIZE / sizeof(uint16_t); i++)
     {
         data[i] = i;
