8987a0558248b796fd1f263694bd1cc02e7d7475
maksyuki <maksyuki@126.com>
2022-10-05 20:38:57 +0800

    feat: modify vga test prog

--- ysyx/prog/src/vga/vga.c
+++ ysyx/prog/src/vga/vga.c
@@ -2,13 +2,24 @@
 #include <klib.h>
 #include <klib-macros.h>
 
-#define VGA_REG_CTRL *((volatile uint32_t *)0x10002000)
-#define VGA_REG_BASE *((volatile uint32_t *)0x10002004)
-#define VGA_REG_OFST *((volatile uint32_t *)0x10002008)
+#define FB_ADDR 0x1c000000
+#define VGA_CTRL_ADDR 0x10002000
+#define VGA_FB_ADDR 0x81000000
+#define VGA_REG_CTRL *((volatile uint32_t *)(VGA_CTRL_ADDR))
+#define VGA_REG_BASE *((volatile uint32_t *)(VGA_CTRL_ADDR + 4))
+#define VGA_REG_OFST *((volatile uint32_t *)(VGA_CTRL_ADDR + 8))
 
 int main()
 {
-    VGA_REG_CTRL = 0;
-    putstr("Hello World!\n");
+    putstr("config VGA module\n");
+    // VGA_REG_CTRL = 0;
+    // VGA_REG_BASE = VGA_FB_ADDR;
+    // VGA_REG_OFST = VGA_FB_ADDR - FB_ADDR;
+
+    // uint32_t write_mode = VGA_REG_CTRL;
+    // uint32_t write_base = VGA_REG_BASE;
+    // uint32_t write_ofst = VGA_REG_OFST;
+    printf("mode: %x base: %x offset: %x\n", 0, 0, 0);
+
     return 0;
 }
